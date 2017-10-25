777996f7694fcf1dbafdaa8364b576e3cec7ef04
nikkleo <32703598+nikkleo@users.noreply.github.com>
2017-10-25 17:31:30 +0300

    Main
    Annos lista

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
+            map.put("annokset", annosDao.findAll());
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
